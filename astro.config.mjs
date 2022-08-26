75d4854c2999a349e0b43676784803c7344024d4
aalhendi <abdulrazaq.alhendi@gmail.com>
2022-08-26 16:29:27 +0300

    chore: package updates and strict types

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
-
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://tinyrocket.pages.dev',
-    integrations: [preact(), tailwind(), sitemap()]
+  site: 'https://aalhendi.com',
+    integrations: [preact(), tailwind(), sitemap(), mdx()],
+  markdown: {
+    drafts: true
+  }
 });
